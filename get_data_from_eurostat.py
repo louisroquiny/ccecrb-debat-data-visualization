44795bd9cda957fe5dc52a14949b16bb94a45c83
louisroquiny <lroquiny@gmail.com>
2023-03-17 14:15:08 +0100

    Update get_data_from_eurostat.py

--- get_data_from_eurostat.py
+++ get_data_from_eurostat.py
@@ -29,6 +29,7 @@ EndPeriod = 2021
 url_expenditure = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/GOV_10A_EXP/A.MIO_EUR+PC_GDP.S13.GF02+GF01+GF04+GF03+GF06+GF05+GF08+GF07+GF09+GF10+GF1001+GF0908+GF0505+GF0703+GF0901+GF0108+GF0306+GF0504+GF0702+GF0705+GF0903+GF0506+GF0704+GF0902+GF0905+GF0706+GF0904+GF0907+GF0906+GF0101+GF0103+GF0301+GF0102+GF0105+GF0303+GF0501+GF0104+GF0302+GF0107+GF0305+GF0503+GF0701+GF0106+GF0304+GF0502+GF1003+GF1002+GF1005+GF1004+GF1007+GF1006+GF1009+GF1008+GF0406+GF0604+GF0802+GF0405+GF0603+GF0801+GF0408+GF0606+GF0804+GF0407+GF0605+GF0803+GF0806+GF0409+GF0805+GF0202+GF0201+GF0204+GF0402+GF0203+GF0401+GF0404+GF0602+GF0205+GF0403+GF0601.TE.DE+NO+BE+FI+PT+BG+DK+LT+LU+HR+LV+FR+HU+SE+SI+SK+EU27_2020+IE+EE+CH+EL+MT+IS+IT+ES+AT+CY+CZ+PL+RO+NL/?format=SDMX-CSV&startPeriod={start}&endPeriod={end}&lang=en'
 url_deficit_debt = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/GOV_10DD_EDPT1/A.MIO_EUR+PC_GDP.S13.B9+GD.EU27_2020+EA19+BE+BG+CZ+DK+DE+EE+IE+EL+ES+FR+HR+IT+CY+LV+LT+LU+HU+MT+NL+AT+PL+PT+RO+SI+SK+FI+SE/?format=SDMX-CSV&startPeriod={start}&endPeriod={end}'
 url_revenue = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/GOV_10A_MAIN/A.MIO_EUR+PC_GDP.S13.B9+TE+TR.EU27_2020+EA20+EA19+BE+BG+CZ+DK+DE+EE+IE+EL+ES+FR+HR+IT+CY+LV+LT+LU+HU+MT+NL+AT+PL+PT+RO+SI+SK+FI+SE+IS+NO+CH/?format=SDMX-CSV&startPeriod={start}&endPeriod={end}'
+url_gdp = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/NAMA_10_GDP/A.CP_MEUR.B1GQ.DE+NO+RS+BE+FI+PT+BG+DK+LT+LU+HR+LV+FR+HU+SE+SI+UK+SK+ME+EU27_2020+IE+MK+EE+CH+EL+MT+IS+IT+AL+ES+AT+XK+CY+CZ+PL+RO+LI+NL+TR+BA/?format=SDMX-CSV&startPeriod={start}&endPeriod={end}'
 
 def dataframed(url) :
     url_formated = url.format(start = StartPeriod, end = EndPeriod)
@@ -39,6 +40,7 @@ def dataframed(url) :
 expenditure = dataframed(url_expenditure)
 deficit_debt = dataframed(url_deficit_debt)
 revenue = dataframed(url_revenue)
+gdp = dataframed(url_gdp)
 
 # Sectorisation
 sector = []
@@ -50,8 +52,8 @@ for s in expenditure.cofog99.unique():
 expenditure = expenditure[['unit', 'sector', 'cofog99','geo', 'TIME_PERIOD', 'OBS_VALUE']]
 expenditure = expenditure[~expenditure.cofog99.isin(sector)]
 deficit_debt = deficit_debt[['unit', 'na_item','geo', 'TIME_PERIOD', 'OBS_VALUE']]
-revenue = revenue[['unit', 'na_item', 'geo',
-       'TIME_PERIOD', 'OBS_VALUE']]
+revenue = revenue[['unit', 'na_item', 'geo', 'TIME_PERIOD', 'OBS_VALUE']]
+gdp = gdp[['geo','TIME_PERIOD', 'OBS_VALUE']]
 
 for s in sector : 
     filt = expenditure.cofog99.str.contains(s)
@@ -60,11 +62,13 @@ for s in sector :
 map_my_dataframe(expenditure, codelist)
 map_my_dataframe(deficit_debt, codelist)
 map_my_dataframe(revenue, codelist)
+map_my_dataframe(gdp, codelist)
 
 # Save new file in cache
 expenditure.to_csv('data/expenditure.csv', sep = ';', index = False)
 deficit_debt.to_csv('data/deficit_debt.csv', sep = ';', index = False)
 revenue.to_csv('data/revenue.csv', sep = ';', index = False)
+gdp.to_csv('data/gdp.csv', sep = ';', index = False)
 
 # new_file = io.StringIO(file_content)
 
